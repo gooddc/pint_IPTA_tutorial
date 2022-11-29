3fd278fb4cdf967bf99e2d0caac98351c3e72090
Deborah Good <deborah.good@nanograv.org>
2022-11-29 12:28:26 -0500

    Initial commit, adding tutorials, sample data, etc.

---
+++ real_IPTA_resources/run_J1744_reflagger.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+python ../../src/timing_analysis/reflagger.py EFF.EBPP.1360.tim EFF.EBPP.1360.reflag.tim epta
+python ../../src/timing_analysis/reflagger.py EFF.EBPP.1410.tim EFF.EBPP.1410.reflag.tim epta
+python ../../src/timing_analysis/reflagger.py EFF.EBPP.2639.tim EFF.EBPP.2639.reflag.tim epta
+python ../../src/timing_analysis/reflagger.py EFF.P217.1400.tim EFF.P217.1400.reflag.tim epta
+python ../../src/timing_analysis/reflagger.py EFF.S110.2639.tim EFF.S110.2639.reflag.tim epta
+python ../../src/timing_analysis/reflagger.py EFF.S60.4857.tim EFF.S60.4857.reflag.tim epta
+python ../../src/timing_analysis/reflagger.py JBO.DFB.1400.tim JBO.DFB.1400.reflag.tim epta
+python ../../src/timing_analysis/reflagger.py JBO.DFB.1520.tim JBO.DFB.1520.reflag.tim epta
+python ../../src/timing_analysis/reflagger.py JBO.ROACH.1520.tim JBO.ROACH.1520.reflag.tim epta
+python ../../src/timing_analysis/reflagger.py LEAP.1396.tim LEAP.1396.reflag tim epta
+python ../../src/timing_analysis/reflagger.py NRT.BON.1400.tim NRT.BON.1400.reflag.tim epta
+python ../../src/timing_analysis/reflagger.py NRT.BON.1600.tim NRT.BON.1600.reflag.tim epta
+python ../../src/timing_analysis/reflagger.py NRT.BON.2000.tim NRT.BON.2000.reflag.tim epta
+python ../../src/timing_analysis/reflagger.py NRT.NUPPI.1484.tim NRT.NUPPI.1484.reflag.tim epta
+python ../../src/timing_analysis/reflagger.py NRT.NUPPI.1854.tim NRT.NUPPI.1854.reflag.tim epta
+python ../../src/timing_analysis/reflagger.py NRT.NUPPI.2539.tim NRT.NUPPI.2539.reflag.tim epta
+python ../../src/timing_analysis/reflagger.py WSRT.P1.323.tim WSRT.P1.323.reflag.tim epta
+python ../../src/timing_analysis/reflagger.py WSRT.P1.367.tim WSRT.P1.367.reflag.tim epta
+python ../../src/timing_analysis/reflagger.py WSRT.P2.1380.tim WSRT.P2.1380.reflag.tim epta
+
+python ../../src/timing_analysis/reflagger.py J1744-1134_PINT_20220302.nb.tim J1744-1134_PINT_20220302.nb.reflag.tim NANOGrav
